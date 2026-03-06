c561cd43bd52594fe2ccf9027d68a95d511bbd55
Brenda Galdamez <jgaldamez098@gmail.com>
2026-03-05 22:27:59 -0500

    Modify GUI with previous assignment

--- src/main/java/com/example/csc325_firebase_webview_auth/model/Person.java
+++ src/main/java/com/example/csc325_firebase_webview_auth/model/Person.java
@@ -16,23 +16,62 @@ package com.example.csc325_firebase_webview_auth.model;
  *
  */
 public class Person {
-    private String name;
-    private String major;
-    private int age;
 
-    public Person(String name, String major, int age) {
-        this.name = name;
+    private String documentId;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String department;
+    private String major;
+    private String email;
+    private String imageURL;
+
+
+    public Person(String firstName, String lastName, int age, String department, String major, String email, String imageURL) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.department = department;
         this.major = major;
+        this.email = email;
+        this.imageURL = imageURL;
+    }
+
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
         this.age = age;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public String getMajor() {
         return major;
@@ -42,14 +81,31 @@ public class Person {
         this.major = major;
     }
 
-    public int getAge() {
-        return age;
+
+    public String getDepartment() {
+        return department;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setDepartment(String department) {
+        this.department = department;
     }
-    
-    
-    
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
 }
+
